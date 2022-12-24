a94380317fbe504858c71b9c222d07ee0b85f534
mjd <mjd.abdullah75@gmail.com>
2022-12-24 13:16:00 +0300

    update project

--- challenge_project/lib/main.dart
+++ challenge_project/lib/main.dart
@@ -24,92 +24,111 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const HomeScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        backgroundColor: Colors.black,
+        title: Text("Challenge Project"),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+      body: SafeArea(
+        child:Column(
+          children: [
+            CustomColumn(),
+            CustomColumn(),
+            CustomColumn()
           ],
         ),
+
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class CustomColumn extends StatelessWidget {
+  const CustomColumn({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Center(
+            child: CustomRow(
+          oneColor: Colors.black,
+          twoColor: Colors.white,
+          threeColor: Colors.black,
+          fourColor: Colors.white,
+        )),
+        Center(
+            child: CustomRow(
+          oneColor: Colors.white,
+          twoColor: Colors.black,
+          threeColor: Colors.white,
+          fourColor: Colors.black,
+        ))
+      ],
+    );
+  }
+}
+
+class CustomRow extends StatelessWidget {
+  const CustomRow({
+    Key? key,
+    required this.oneColor,
+    required this.twoColor,
+    required this.threeColor,
+    required this.fourColor,
+  }) : super(key: key);
+
+  final Color oneColor;
+  final Color twoColor;
+  final Color threeColor;
+  final Color fourColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        CustomContainer(colorContainer: oneColor),
+        CustomContainer(colorContainer: twoColor),
+        CustomContainer(colorContainer: threeColor),
+        CustomContainer(colorContainer: fourColor)
+      ],
+    );
+  }
+}
+
+class CustomContainer extends StatelessWidget {
+  const CustomContainer({
+    Key? key,
+    required this.colorContainer,
+  }) : super(key: key);
+
+  final Color colorContainer;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      //decoration: BoxDecoration(),
+      child: Center(
+          child: Text("Hi",
+              style: TextStyle(
+                  fontSize: 36,
+                  color: Colors.cyanAccent,
+                  fontWeight: FontWeight.w800))),
+      color: colorContainer,
+      height: 100,
+      width: 98,
     );
   }
 }
